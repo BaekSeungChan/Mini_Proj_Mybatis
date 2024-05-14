a752cdadb2f1de1f82ecc0dfdc398e22fe0225bd
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-05-14 23:40:06 +0900

    merge 충돌 해결

--- src/main/java/com/example/minproj2_mybatis/config/SecurityConfig.java
+++ src/main/java/com/example/minproj2_mybatis/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package com.example.minproj2_mybatis.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import com.example.minproj2_mybatis.jwt.LoginFilter;
+import com.example.minproj2_mybatis.member.service.MemberService;
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +28,7 @@ public class SecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
         return configuration.getAuthenticationManager();
     }
 
@@ -36,34 +41,49 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf((csrf) -> csrf.disable())
-                .formLogin((form) -> form.disable())
-                .httpBasic((basic) -> basic.disable());
+                .csrf((auth) -> auth.disable())
+                .formLogin((auth) -> auth.disable())
+                .httpBasic((auth) -> auth.disable());
 
-        http
-                .logout(logout -> logout
-                        .logoutUrl("/member/logout")
-                        .logoutSuccessUrl("/")
-                        .deleteCookies("JSESSIONID")
-                );
+//        http.formLogin(form -> form
+//                .loginPage("/member/login")  // 사용자 정의 로그인 페이지 경로 설정
+////                .successHandler(customAuthentiactionSuccesHandler)
+////                .defaultSuccessUrl("/board/list")  // 로그인 성공 시 리다이렉트 될 기본 URL 설정
+//                .failureUrl("/member/login/error")  // 로그인 실패 시 리다이렉트 될 URL 설정
+//                .usernameParameter("email")  // 로그인 폼에서 사용할 사용자 이름 파라미터 이름 설정
+//                .passwordParameter("password")  // 로그인 폼에서 사용할 비밀번호 파라미터 이름 설정
+//                .permitAll());  // 모든 사용자가 접근할 수 있도록 설정
 
-        http
-                .authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .dispatcherTypeMatchers(DispatcherType.INCLUDE).permitAll()
-                        .requestMatchers("/css/**").permitAll()
-                        .requestMatchers("/", "/member/**", "/board/list", "/volume-rank", "/finance/**", "/gpt/**", "/admin/**").permitAll()
-                        .requestMatchers("/admin/delete/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                );
+        http.logout(logoutConfig ->
+                        logoutConfig
+                                .logoutUrl("/member/logout")
+                                .logoutSuccessUrl("/")
+                                .deleteCookies("JSESSIONID")
+//                        .logoutSuccessHandler().
+//                        .addLogoutHandler()
+        );
+
+        http.authorizeHttpRequests(request -> request
+                .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                .dispatcherTypeMatchers(DispatcherType.INCLUDE).permitAll()
+                .requestMatchers("/css/**").permitAll()
+                .requestMatchers("/","/member/**", "/board/list","/volume-rank", "/finance/**","/gpt/**", "/admin/**").permitAll()
+                .requestMatchers("/admin/delete/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
 
         http
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
 
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/minproj2_mybatis/member/service/MemberService.java
+++ src/main/java/com/example/minproj2_mybatis/member/service/MemberService.java
@@ -1,57 +1,64 @@
 package com.example.minproj2_mybatis.member.service;
 
-
+import com.example.minproj2_mybatis.member.dto.MemberDTO;
 import com.example.minproj2_mybatis.member.entity.MemberEntity;
 import com.example.minproj2_mybatis.member.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService{
+public class MemberService implements UserDetailsService {
 
     private final MemberMapper memberMapper;
 
-    public int save(MemberEntity member){
 
-        duplicatedforCheck(member);
+    public int save(MemberEntity memberDTO){
+        validateDuplicationMember(memberDTO);
+        return memberMapper.save(memberDTO);
+    }
 
-        return memberMapper.save(member);
-
+    public MemberDTO findMember(String email){
+        return null;
     }
 
 
-    private void duplicatedforCheck(MemberEntity member){
-        if (memberMapper.existsfindByEmail(member.getEmail())) {
-            throw new IllegalArgumentException("이미 존재하는 아이디 입니다.");
+    private void validateDuplicationMember(MemberEntity member){
+        Optional<MemberEntity> findMember = memberMapper.findByEmail(member.getEmail());
+
+        if(findMember.isPresent()){
+            throw new IllegalStateException("이미 존재하는 이메일입니다.");
         }
     }
 
-//    @Override
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        MemberEntity member = memberMapper.findByEmail(email)
-//                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 없습니다."));
-//
-//        System.out.println(member);
-//
-//        if(member == null){
-//            throw new UsernameNotFoundException(email + "을 찾을 수 없습니다.");
-//        }
-//
-//        if(!"Y".equals(member.getIsUsed())){
-//            throw new UsernameNotFoundException("사용할 수 없는 계정입니다.");
-//        }
-//
-//        if(!"N".equals(member.getIsDel())){
-//            throw new UsernameNotFoundException("삭제된 계정입니다.");
-//        }
-//
-//        return null;
-//    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        MemberEntity member = memberMapper.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 없습니다."));
+
+        System.out.println(member);
+
+        if(member == null){
+            throw new UsernameNotFoundException(email + "을 찾을 수 없습니다.");
+        }
+
+        if(!"Y".equals(member.getIsUsed())){
+            throw new UsernameNotFoundException("사용할 수 없는 계정입니다.");
+        }
+
+        if(!"N".equals(member.getIsDel())){
+            throw new UsernameNotFoundException("삭제된 계정입니다.");
+        }
+
+        return null;
+    }
 
 }
